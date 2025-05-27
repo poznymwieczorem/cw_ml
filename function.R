da545afe18958f6099e372cab74763994d415f81
Fabian <fwieczorek@student.agh.edu.pl>
2025-05-27 18:42:43 +0200

    update function.R

--- function.R
+++ function.R
@@ -1,9 +1,30 @@
+library(ggplot2)
 # funkcja
 
 new_f <- function(x){
   
-  y <- x / 100 + sqrt(x)
+  if (!is.numeric(x)) { stop("Obiekt wejściowy x nie jest typu numeric") }
+  
+  srednia <- mean(x)
+  
+  y <- srednia / 100 + sqrt(srednia)
   
   return(y)
   
 }
+
+# test 1 funkcji 
+
+dane <- 1:10
+
+new_f(x = dane)
+
+# test 2 - sprawdzamy, czy odporna
+
+new_f(x = letters)
+
+letters |> str()
+
+# test 3 - wykres
+
+ggplot(aes(x=x, y=y)) + plot(x ,y)
